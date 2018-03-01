4db7514477c5cbe5472a62e895a043c61a010c35
Aleksbgbg <kozichka01@abv.bg>
2018-03-01 18:46:45 +0000

    Add logger class for uniform logging.

---
+++ ImageWatcher/Core/Logger.cs
@@ -0,0 +1,19 @@
+﻿namespace ImageWatcher.Core
+{
+    using System;
+
+    using static System.Console;
+
+    internal static class Logger
+    {
+        internal static void Log(string message)
+        {
+            WriteLine("[{0}] {1}\n", DateTime.Now, message);
+        }
+
+        internal static void Log(string solutionName, string projectName, string message)
+        {
+            WriteLine("[{0}] /{1}/{2}\n{{\n\t{3}\n}}\n", DateTime.Now, solutionName, projectName, message);
+        }
+    }
+}
