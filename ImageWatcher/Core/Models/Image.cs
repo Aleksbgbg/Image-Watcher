839f1f156ec472e2aca986ca2616b2117c18b38e
Aleksbgbg <kozichka01@abv.bg>
2018-03-01 20:53:47 +0000

    Add Image class to represent images.

---
+++ ImageWatcher/Core/Models/Image.cs
@@ -0,0 +1,31 @@
+﻿namespace ImageWatcher.Core.Models
+{
+    using System.IO;
+
+    internal class Image
+    {
+        private string _resourceString;
+
+        internal Image(string filepath)
+        {
+            GenerateResourceString(filepath);
+        }
+
+        public override string ToString()
+        {
+            return _resourceString;
+        }
+
+        internal void Rename(string newpath)
+        {
+            GenerateResourceString(newpath);
+        }
+
+        private void GenerateResourceString(string filepath)
+        {
+            string filename = Path.GetFileName(filepath);
+
+            _resourceString = $"<BitmapImage x:Key=\"{Path.GetFileNameWithoutExtension(filename)}\" UriSource=\"/Images/{filename}\"/>";
+        }
+    }
+}
