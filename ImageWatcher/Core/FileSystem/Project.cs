c0ead41acf9eb6dfdd3f4213ac971cad078dd8bb
Aleksbgbg <kozichka01@abv.bg>
2018-03-01 19:00:08 +0000

    Modify file system to log various events.

--- ImageWatcher/Core/FileSystem/Project.cs
+++ ImageWatcher/Core/FileSystem/Project.cs
@@ -9,10 +9,17 @@
 
     internal class Project
     {
+        private readonly string _solutionName;
+
+        private readonly string _name;
+
         private readonly ResourceDictionary _imageDictionary = new ResourceDictionary();
 
-        internal Project(string directory)
+        internal Project(string solutionName, string directory)
         {
+            _solutionName = solutionName;
+            _name = Path.GetFileName(directory);
+
             XmlWriter imagesWriter = XmlWriter.Create(Path.Combine(directory, "Themes", "Images.xaml"));
 
             void SaveImageDictionary()
@@ -25,7 +32,7 @@
                 string filename = Path.GetFileName(fullpath);
 
                 _imageDictionary.Add(Path.GetFileNameWithoutExtension(filename),
-                                     new BitmapImage(new Uri($"/Images/{filename}")));
+                                     new BitmapImage(new Uri($"/Images/{filename}", UriKind.Relative)));
             }
 
             string imagesDirectory = Path.Combine(directory, "Images");
@@ -41,29 +48,42 @@
 
             projectWatcher.Created += (sender, e) =>
             {
-                AddNewImage(e.FullPath);
+                string filename = Path.GetFileName(e.FullPath);
+
+                AddNewImage(filename);
 
                 SaveImageDictionary();
+
+                Logger.Log(_solutionName, _name, $"Added image: '{filename}'");
             };
             projectWatcher.Deleted += (sender, e) =>
             {
-                _imageDictionary.Remove(Path.GetFileNameWithoutExtension(e.FullPath));
+                string filename = Path.GetFileName(e.FullPath);
+
+                _imageDictionary.Remove(Path.GetFileNameWithoutExtension(filename));
 
                 SaveImageDictionary();
+
+                Logger.Log(_solutionName, _name, $"Deleted image: '{filename}'");
             };
             projectWatcher.Renamed += (sender, e) =>
             {
-                string oldFilename = Path.GetFileNameWithoutExtension(e.OldFullPath);
+                string oldFilename = Path.GetFileName(e.OldFullPath);
+                string oldFilenameWithoutExtension = Path.GetFileNameWithoutExtension(oldFilename);
                 string newFilename = Path.GetFileName(e.FullPath);
 
-                BitmapImage image = (BitmapImage)_imageDictionary[oldFilename];
-                image.UriSource = new Uri($"/Images/{newFilename}");
+                BitmapImage image = (BitmapImage)_imageDictionary[oldFilenameWithoutExtension];
+                image.UriSource = new Uri($"/Images/{newFilename}", UriKind.Relative);
 
-                _imageDictionary.Remove(oldFilename);
+                _imageDictionary.Remove(oldFilenameWithoutExtension);
                 _imageDictionary.Add(Path.GetFileNameWithoutExtension(newFilename), image);
 
                 SaveImageDictionary();
+
+                Logger.Log(_solutionName, _name, $"Renamed image: '{oldFilename}' -> '{newFilename}");
             };
+
+            Logger.Log($"Registered /{_solutionName}/{_name}");
         }
     }
 }
--- ImageWatcher/Core/FileSystem/Solution.cs
+++ ImageWatcher/Core/FileSystem/Solution.cs
@@ -5,15 +5,19 @@
 
     internal class Solution
     {
+        private readonly string _name;
+
         private readonly Project[] _projects;
 
         internal Solution(string path)
         {
+            _name = Path.GetFileName(path);
+
             _projects = Directory.GetDirectories(path)
                                  .Where(directory => File.Exists(Path.Combine(directory, $"{Path.GetFileName(directory)}.csproj")) &&
                                                      Directory.Exists(Path.Combine(directory, "Images")) &&
                                                      File.Exists(Path.Combine(directory, "Themes", "Images.xaml")))
-                                 .Select(directory => new Project(directory))
+                                 .Select(directory => new Project(_name, directory))
                                  .ToArray();
         }
     }
