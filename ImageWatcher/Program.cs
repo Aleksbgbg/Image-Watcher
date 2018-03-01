621ffae93dab9432350d2ad62034aeb5e8448111
Aleksbgbg <kozichka01@abv.bg>
2018-03-01 18:42:09 +0000

    Instantiate all solutions in C# folder.

--- ImageWatcher/Program.cs
+++ ImageWatcher/Program.cs
@@ -1,12 +1,21 @@
 ﻿namespace ImageWatcher
 {
-    using System;
-    using static System.Console;
+    using System.IO;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using ImageWatcher.Core.FileSystem;
 
     internal static class Program
     {
-        private static void Main()
+        private static async Task Main()
         {
+            Solution[] solutions = Directory.GetDirectories(@"E:\Programming\C#")
+                                            .Where(directory => File.Exists(Path.Combine(directory, $"{Path.GetFileName(directory)}.sln")))
+                                            .Select(directory => new Solution(directory))
+                                            .ToArray();
+
+            await Task.Delay(-1);
         }
     }
 }
