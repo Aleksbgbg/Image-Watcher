c7891a1b3ae0c192b275b5eee5f1d33b6542d76c
Aleksbgbg <kozichka01@abv.bg>
2018-03-02 18:41:50 +0000

    Add FileSystemWatchers to Project to make sure the project is valid (for watching), otherwise Unregister.

--- ImageWatcher/Core/FileSystem/Project.cs
+++ ImageWatcher/Core/FileSystem/Project.cs
@@ -11,12 +11,42 @@
 
         private readonly ImageFolderWatcher _imageFolderWatcher;
 
+        private readonly FileSystemWatcher _folderValidityWatcher;
+
+        private readonly FileSystemWatcher _themesValidityWatcher;
+
         internal Project(string solutionName, string directory)
         {
             _solutionName = solutionName;
             _name = Path.GetFileName(directory);
-
             _imageFolderWatcher = new ImageFolderWatcher(_solutionName, _name, directory);
+
+            _folderValidityWatcher = new FileSystemWatcher(directory) { EnableRaisingEvents = true };
+            _themesValidityWatcher = new FileSystemWatcher(Path.Combine(directory, "Themes"), "Images.xaml") { EnableRaisingEvents = true };
+
+            void UnregisterIfInvalid()
+            {
+                if (!IsValid(directory))
+                {
+                    Unregister();
+                }
+            }
+
+            void WatcherDeleted(object sender, FileSystemEventArgs e)
+            {
+                UnregisterIfInvalid();
+            }
+
+            void WatcherRenamed(object sender, RenamedEventArgs e)
+            {
+                UnregisterIfInvalid();
+            }
+
+            _folderValidityWatcher.Deleted += WatcherDeleted;
+            _folderValidityWatcher.Renamed += WatcherRenamed;
+
+            _themesValidityWatcher.Deleted += WatcherDeleted;
+            _themesValidityWatcher.Renamed += WatcherRenamed;
 
             Logger.Log($"Registered /{_solutionName}/{_name}");
         }
@@ -50,6 +80,8 @@
             if (disposing)
             {
                 _imageFolderWatcher.Dispose();
+                _folderValidityWatcher.Dispose();
+                _themesValidityWatcher.Dispose();
             }
         }
     }
